9aa5587749b64209670870411d56f60df439a403
unknown <ful19017@byui.edu>
2023-04-20 21:33:51 -0600

    Created formatted name return program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,15 @@ using System;
 
 class Program
 {
+    // Takes name from user and displays it in a James Bond style format
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? "); // get first and last name
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? "); // take their answer in on the same line
+        string lastName = Console.ReadLine();
+
+        // display their name in a James Bond format, double-spaced from previous line
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
