ff35dfacdad6ab2ad9bf5915a721c8f53d263a99
danjfuller <ful19017@byui.edu>
2023-05-17 22:12:08 -0600

    Added a fraction class with display methods

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        List<Fraction> fracs = new List<Fraction> // make a list of each new fraction
+        {
+            new Fraction(),
+            new Fraction(5),
+            new Fraction(3,4),
+            new Fraction(1,3),
+            new Fraction(9,21)
+        };
+
+        foreach( Fraction f in fracs)
+        {
+            Console.WriteLine(f.GetDecimalValue()); // write each fraction to the console in 2 different formats
+            Console.WriteLine(f.GetFractionString());
+        }
     }
 }
