8fd49f48dec5c184a62046a5ca756b88c7637bae
danjfuller <ful19017@byui.edu>
2023-07-10 11:29:47 -0600

    Added Gtk GUI test

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,38 @@
+using Gtk;
 using System;
 
-class Program
+public class GtkHelloWorld
 {
-    static void Main(string[] args)
+
+    public static void Main()
     {
-        Console.WriteLine("Hello FinalProject World!");
+        Application.Init();
+
+        //Create the Window
+        Window myWin = new Window("Orbital Collision Simulator");
+        myWin.Resize(1360, 780);
+
+        // allow use to close the window and program
+        myWin.DeleteEvent += delete_event;
+
+        //Create a label and put some text in it.
+        Label myLabel = new Label();
+        myLabel.Text = "Hello World!!!!";
+
+        //Add the label to the form
+        myWin.Add(myLabel);
+
+        //Show Everything
+        myWin.ShowAll();
+
+        Application.Run();
     }
+
+    // runs when the user deletes the window using the "close
+    // window" widget in the window frame.
+    static void delete_event(object obj, DeleteEventArgs args)
+    {
+        Application.Quit();
+    }
+
 }
