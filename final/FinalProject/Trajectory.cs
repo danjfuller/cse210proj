23c98897f75fbe17022d1eedf2ce638f57c811d2
danjfuller <ful19017@byui.edu>
2023-07-10 17:31:31 -0600

    moved classes to correct Project folder

---
+++ final/FinalProject/Trajectory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbitalCollisions
+{
+    class Trajectory
+    {
+        private List<Vector> _path;
+        public Trajectory()
+        {
+            _path = new List<Vector>();
+        }
+
+        private float WhenCollide(Trajectory otherPath)
+        {
+            return 0.0f;
+        }
+
+        public void Plot()
+        {
+            return;
+        }
+
+        public List<Vector> NextSpots()
+        {
+            return _path.GetRange(0, 5); // next 5 points for now
+        }
+
+        public bool WillCollide()
+        {
+            return false;
+        }
+    }
+}
