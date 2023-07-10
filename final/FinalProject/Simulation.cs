195165cc892ddddc653aa287676fb9cfd3eb9ffd
danjfuller <ful19017@byui.edu>
2023-07-10 17:34:23 -0600

    removed unused library references

--- final/FinalProject/Simulation.cs
+++ final/FinalProject/Simulation.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
 
 namespace OrbitalCollisions
 {
