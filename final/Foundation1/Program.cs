d5cc29ccf218b3611bdebf177b7ac63782b5cbce
danjfuller <ful19017@byui.edu>
2023-10-11 21:02:55 -0600

    Removed extra files

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using Gtk;
 
+// This is a test for the GTK library
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Application.Init();
+
+        //Create the Window
+        Window myWin = new Window("Orbital Collision Simulator");
+        myWin.Resize(780, 780);
+
+        // allow use to close the window and program
+
+        //Create a label and put some text in it.
+        Label myLabel = new Label();
+        myLabel.Text = "Hello World!!!!";
+
+        //Add the label to the form
+        myWin.Add(myLabel);
+
+        //Show Everything
+        myWin.ShowAll();
+
+        Application.Run();
     }
 }
